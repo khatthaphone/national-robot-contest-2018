6608e7a4fbe1b466b2c5ac58ea9b10200b187b92
Khatthaphone <khatthaphonez@gmail.com>
2018-02-11 20:48:25 +0700

    working on auto cycle loop

--- src/auto/test_state_line_following/test_state_line_following.ino
+++ src/auto/test_state_line_following/test_state_line_following.ino
@@ -39,7 +39,28 @@ int pwmTurn_r = 35;
 int pwmUturn_l = 60;
 int pwmUturn_r = 60;
 
+// states of the robot
+#define START 0
+#define WAIT 1
+#define GO 2
+#define DROP_IN 3
+#define DROP_OUT 4
+#define BACK 5
 
+// colors
+#define GREEN 1
+#define RED 0
+#define BLUE 2
+
+// state
+int state = START;
+// color
+int color = GREEN;
+// count for checkpoint count
+int count = 0;
+
+// TODO: Infared sensor to know if the robot have a box
+int infared = 0;
 
 void setup() {
   Serial.begin(250000);
@@ -61,10 +82,91 @@ void setup() {
 } //Ending setup fucntion
 
 void loop() {
+
+  switch(state) {
+    case START:
+      controlFollowLine(); //FollowLine
+      if (detectCheckpoint()) {
+        delay(225);
+        state = WAIT;
+      }
+      break;
+    case WAIT:
+      forward(0, 0);
+      
+      // replace with wait for infared sonsor
+        delay(1000);
+        
+      forward(pwm_l, pwm_r);
+      delay(200);
+      state = GO;
+      
+      break;
+    case GO:
+      controlFollowLine(); //FollowLine
+      if (detectCheckpoint()) {
+        delay(100);
+        ++count;
+      }
+      // count == count to go for ccolor
+      if (count == goCheckpointCount(color)) {
+        delay(500);
+        count = 0;
+        state = DROP_IN;
+      }
+      break;
+      
+    case DROP_IN:
+      forward(0, 0);
+      delay(200);
+
+      // turn left to drop
+      uturnLeftZone(pwmUturn_l, pwmUturn_r);
+      delay(200);
+
+      // stop when turn succeed
+      if (dropInTurnSucceed())
+      {
+        state = DROP_OUT;
+      }
+      
+      break;
+      
+    case DROP_OUT:
+      forward(0, 0);
+      delay(200);
+
+      // turn right to get out
+      uturnRightZone(pwmUturn_l, pwmUturn_r);
+      delay(200);
+      
+      // stop when turn succeed
+      if (!dropInTurnSucceed())
+      {
+        forward(pwm_l, pwm_r);
+        delay(200);
+        state = BACK;
+      }
+      break;
+      
+    case BACK:
+
+      controlFollowLine(); //FollowLine
+      if (detectCheckpoint()) {
+        delay(100);
+        ++count;
+      }
+      if (count == backCheckpointCount(color)) {
+        state = WAIT;
+        color = changeToNextColor(color);
+        count = 0; 
+      }
+
+      break;
+  }
   
-    controlFollowLine(); //FollowLine
-    control_Sensor(); //Read Data From Sensor
-    count_Line(); // Count Line when sensor_1 ==> sensor_5 == HIGH
+//    controlFollowLine(); //FollowLine
+//    control_Sensor(); //Read Data From Sensor
     
 //  Serial.print("sensorT1 : ");
 //  Serial.println(data_sensor_1);
@@ -101,7 +203,9 @@ void loop() {
 // TODO: sensorT3 is not working must change the new once
 void controlFollowLine() 
 {
-      // alway moving
+      control_Sensor();
+      
+      // always moving
       if ((data_sensor_6 == 0) && (data_sensor_1 == 0) && (data_sensor_2 == 1) && (data_sensor_3 == 1) && (data_sensor_4 == 1)&& (data_sensor_5 == 0) && (data_sensor_7 == 0))
       { 
               forward(pwm_l, pwm_r);
@@ -171,7 +275,6 @@ void controlFollowLine()
         uturnRightZone(pwmUturn_l + 15, pwmUturn_r + 5);
         delay(40);
       }
-
       //turn left at the uturn zone
        else if ((data_sensor_6 == 0) && (data_sensor_1 == 1) && (data_sensor_2 == 1) && (data_sensor_3 == 1) && (data_sensor_4 == 1)&& (data_sensor_5 == 0) && (data_sensor_7 == 0))
       {
@@ -215,21 +318,34 @@ void controlFollowLine()
 } //end control follow line
 
 
-void count_Line()
+bool detectCheckpoint()
   {
+    bool isAtCheckpoint = false;
+    
     if(data_sensor_1 != last_state_sensor  && data_sensor_2 != last_state_sensor && data_sensor_3 != last_state_sensor && data_sensor_4 != last_state_sensor && data_sensor_5 != last_state_sensor)
     {
       if(data_sensor_1 == 1 && data_sensor_2 == 1 && data_sensor_3 == 1 && data_sensor_4 == 1 && data_sensor_5 == 1)
       {
-        count_sensor++;
+        isAtCheckpoint = true;
         Serial.print("Line: ");
         Serial.println(count_sensor);
-        delay(10);
       }
-      delay(20);
     }
-    last_state_sensor = data_sensor_1 && data_sensor_2 && data_sensor_3 &&  data_sensor_4 && data_sensor_5;  
+    last_state_sensor = data_sensor_1 && data_sensor_2 && data_sensor_3 &&  data_sensor_4 && data_sensor_5;
+
+    return isAtCheckpoint;
   }
+
+bool dropInTurnSucceed() {
+  control_Sensor();
+
+  if ((data_sensor_6 == 0) && (data_sensor_1 == 0) && (data_sensor_2 == 1) && (data_sensor_3 == 1) && (data_sensor_4 == 1)&& (data_sensor_5 == 0) && (data_sensor_7 == 0))
+  {
+    return true;
+  }
+
+  return false;
+}
 
 void forward(int pwm_l, int pwm_r)
 {
@@ -271,3 +387,27 @@ void uturnRightZone (int pwm_l, int pwm_r)
   digitalWrite(dir2, HIGH);
   analogWrite(pwm2, pwm_r);
 }
+
+int goCheckpointCount(int color) {
+  switch(color) {
+    case GREEN: return 2;
+    case BLUE: return 3;
+    case RED: return 1;
+  }
+}
+
+int backCheckpointCount(int color) {
+  switch(color) {
+    case GREEN: return 2;
+    case BLUE: return 1;
+    case RED: return 3;
+  }
+}
+int changeToNextColor(int color) {
+    switch(color) {
+    case GREEN: return BLUE;
+    case BLUE: return RED;
+    case RED: return GREEN;
+  }
+}
+
